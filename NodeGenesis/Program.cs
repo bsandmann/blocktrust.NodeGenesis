a30774e3bbc31529884c2cd06aaceff4ebf88791
Björn Sandmann <sandmann@codedata.solutions>
2024-07-11 14:52:39 +0200

    Another path fix

--- NodeGenesis/Program.cs
+++ NodeGenesis/Program.cs
@@ -142,7 +142,7 @@ class Program
             outputContent = outputContent.Replace("__IndyscanDaemonUiImage__", config.IndyscanDaemonUiImage);
 
             var workerPaths = config.Networks.Where(p => p.Add)
-                                             .Select(network =>$"/home/indyscan/indyscan-daemon/{network.NameAndFixedId}.json");
+                                             .Select(network =>$"/home/indyscan/indyscan-daemon/app-configs-docker/{network.NameAndFixedId}.json");
             var workerPathString = string.Join(",", workerPaths);
             outputContent = outputContent.Replace("__WorkerConfigs__", workerPathString);
 
